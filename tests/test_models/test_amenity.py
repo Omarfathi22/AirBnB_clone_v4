3aab9a4236ed281311cd62795bfb11260da155e5
Omar Fathi22 <ommarf525@gmail.com>
2024-08-11 13:23:28 -0700

    clone_v4

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-This module contains the TestAmenityDocs classes,
-which are used to test the documentation and code style of the Amenity class.
+Contains the TestAmenityDocs and TestAmenity classes for testing the Amenity class.
 """
 
 from datetime import datetime
@@ -15,55 +14,55 @@ Amenity = amenity.Amenity
 
 
 class TestAmenityDocs(unittest.TestCase):
-    """Class for testing the documentation and style of the Amenity class"""
+    """Tests for checking the documentation and style of the Amenity class."""
     
     @classmethod
     def setUpClass(cls):
-        """Set up for the doc tests by collecting all the functions in the Amenity class"""
+        """Set up method for docstring tests, retrieves all functions of Amenity."""
         cls.amenity_f = inspect.getmembers(Amenity, inspect.isfunction)
 
     def test_pep8_conformance_amenity(self):
-        """Test that models/amenity.py conforms to PEP8 style guidelines"""
+        """Test if models/amenity.py conforms to PEP8 style guidelines."""
         pep8s = pep8.StyleGuide(quiet=True)
         result = pep8s.check_files(['models/amenity.py'])
         self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings) in amenity.py")
+                         "Found code style errors (and warnings).")
 
     def test_pep8_conformance_test_amenity(self):
-        """Test that tests/test_models/test_amenity.py conforms to PEP8 style guidelines"""
+        """Test if tests/test_models/test_amenity.py conforms to PEP8 style guidelines."""
         pep8s = pep8.StyleGuide(quiet=True)
         result = pep8s.check_files(['tests/test_models/test_amenity.py'])
         self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings) in test_amenity.py")
+                         "Found code style errors (and warnings).")
 
     def test_amenity_module_docstring(self):
-        """Test that the amenity.py module has a docstring"""
+        """Test if amenity.py module has a docstring."""
         self.assertIsNot(amenity.__doc__, None,
-                         "amenity.py module needs a docstring")
+                         "amenity.py needs a docstring")
         self.assertTrue(len(amenity.__doc__) >= 1,
-                        "amenity.py module docstring is too short")
+                        "amenity.py needs a docstring")
 
     def test_amenity_class_docstring(self):
-        """Test that the Amenity class has a docstring"""
+        """Test if the Amenity class has a docstring."""
         self.assertIsNot(Amenity.__doc__, None,
                          "Amenity class needs a docstring")
         self.assertTrue(len(Amenity.__doc__) >= 1,
-                        "Amenity class docstring is too short")
+                        "Amenity class needs a docstring")
 
     def test_amenity_func_docstrings(self):
-        """Test that all methods in the Amenity class have docstrings"""
+        """Test if all methods of the Amenity class have docstrings."""
         for func in self.amenity_f:
             self.assertIsNot(func[1].__doc__, None,
-                             "{:s} method needs a docstring".format(func[0]))
+                             "{} method needs a docstring".format(func[0]))
             self.assertTrue(len(func[1].__doc__) >= 1,
-                            "{:s} method docstring is too short".format(func[0]))
+                            "{} method needs a docstring".format(func[0]))
 
 
 class TestAmenity(unittest.TestCase):
-    """Class for testing the functionality of the Amenity class"""
+    """Tests for the functionality of the Amenity class."""
     
     def test_is_subclass(self):
-        """Test if Amenity is a subclass of BaseModel and has the expected attributes"""
+        """Test if Amenity is a subclass of BaseModel."""
         amenity = Amenity()
         self.assertIsInstance(amenity, BaseModel)
         self.assertTrue(hasattr(amenity, "id"))
@@ -71,7 +70,7 @@ class TestAmenity(unittest.TestCase):
         self.assertTrue(hasattr(amenity, "updated_at"))
 
     def test_name_attr(self):
-        """Test that the Amenity has an attribute 'name', and it is initialized correctly"""
+        """Test if Amenity has a 'name' attribute, and it is an empty string or None."""
         amenity = Amenity()
         self.assertTrue(hasattr(amenity, "name"))
         if models.storage_t == 'db':
@@ -80,9 +79,8 @@ class TestAmenity(unittest.TestCase):
             self.assertEqual(amenity.name, "")
 
     def test_to_dict_creates_dict(self):
-        """Test that the to_dict method creates a dictionary with the correct attributes"""
+        """Test if to_dict method creates a dictionary with the correct attributes."""
         am = Amenity()
-        print(am.__dict__)  # Debug print statement to view the __dict__ of the Amenity object
         new_d = am.to_dict()
         self.assertEqual(type(new_d), dict)
         self.assertFalse("_sa_instance_state" in new_d)
@@ -92,7 +90,7 @@ class TestAmenity(unittest.TestCase):
         self.assertTrue("__class__" in new_d)
 
     def test_to_dict_values(self):
-        """Test that the values in the dictionary returned from to_dict are correct"""
+        """Test if the values in the dictionary returned by to_dict are correct."""
         t_format = "%Y-%m-%dT%H:%M:%S.%f"
         am = Amenity()
         new_d = am.to_dict()
@@ -103,7 +101,7 @@ class TestAmenity(unittest.TestCase):
         self.assertEqual(new_d["updated_at"], am.updated_at.strftime(t_format))
 
     def test_str(self):
-        """Test that the __str__ method produces the correct output"""
+        """Test if the __str__ method returns the correct string representation of Amenity."""
         amenity = Amenity()
         string = "[Amenity] ({}) {}".format(amenity.id, amenity.__dict__)
         self.assertEqual(string, str(amenity))
